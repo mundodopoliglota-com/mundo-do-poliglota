66b26de4e4d6b6e4ded716b0a40042141a9eb842
John Pontes <jpontes@questrade.com>
2025-05-16 18:52:06 -0300

    Adiciona o site Mundo do Poliglota

--- scripts/script.js
+++ scripts/script.js
@@ -7,3 +7,18 @@ toggleBtn.onclick = function () {
     const isOpen = dropDownMenu.classList.contains('open');
     toggleBtnIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
 };
+
+let lastScroll = 0;
+const btn = document.getElementById('back-to-top');
+window.addEventListener('scroll', function() {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll > 200) {
+    btn.style.display = 'block';
+  } else {
+    btn.style.display = 'none';
+  }
+  lastScroll = currentScroll;
+});
+btn.addEventListener('click', function() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
